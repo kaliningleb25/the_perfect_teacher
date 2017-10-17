2212f27383cbd5fc3d6fd893f2fb9deaeb3ae66e
Глеб Калинин <kaliningleb25@gmail.com>
2017-10-17 19:11:31 +0300

    backup before test with several students

--- game/Dialog.gd
+++ game/Dialog.gd
@@ -1,26 +1,61 @@
 extends Control
 
-#onready var dialog = get_node("Control/dialog")
-#onready var question = get_node("question")
-#onready var b_right = get_node("Control/dialog/b_right")
-#onready var b_wrong = get_node("Control/dialog/b_wrong")
-
+onready var dialog = get_node("dialog")
 onready var question = get_node("dialog/question")
+onready var b_correct = get_node("dialog/b_correct")
+onready var b_wrong = get_node("dialog/b_wrong")
+onready var score = get_node("score")
+
+onready var scr_count = 0
+
+onready var check = -1
 
 onready var questions = {
-"Some question" : 0,
-"Another question" : 1
+"A cat has five legs" : 0,
+"People have two eyes" : 1 
 }
 
 onready var qst = questions.keys()
+onready var rand_index
+onready var string
 
 func get_question():
 	randomize()
-	question.set_text(qst[randi()%qst.size()])
+	rand_index = randi()%qst.size()
+	string = qst[rand_index]
+	question.set_text(string)
+
+func answer():
+	if (b_correct.is_pressed()):
+		check = 1
+		dialog.hide()
+	if (b_wrong.is_pressed()):
+		check = 0
+		dialog.hide()
+	
+	return check
+
+func check_answer():
+
+	answer()
+	#print("wait for answer: ", questions[string])
+	#print("check ", check)
+	
+	if (questions[string] == answer()):
+		scr_count = 1
+		score.set_text("Score: " + str(scr_count))
+	#TODO else: GAMEOVER!
+
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	get_question()
 	
+	
+
 	set_process(true)
+	
+func _process(delta):
+	check_answer()
+	
--- game/university.gd
+++ game/university.gd
@@ -9,6 +9,8 @@ var screen_size
 var teacher_size
 var direction = Vector2(1.0, 0.0)
 
+onready var students = get_tree().get_nodes_in_group("students")
+
 
 #Constant for student speed (in pixels/second)
 const INITIAL_STUDENT_SPEED = 280
@@ -16,43 +18,83 @@ const INITIAL_STUDENT_SPEED = 280
 var student_speed = INITIAL_STUDENT_SPEED
 
 onready var dialog = get_node("Control/dialog")
-
-onready var b_right = get_node("Control/dialog/b_right")
+onready var b_correct = get_node("Control/dialog/b_correct")
 onready var b_wrong = get_node("Control/dialog/b_wrong")
+
+onready var check = 0
+
+onready var stud_array = []
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	screen_size = get_viewport_rect().size
 	teacher_size = get_node("teacher").get_texture().get_size()
-
+#	for s in students:
+#		stud_array.append(s)
+		
+	#var students_pos = []
+	#for i in range(0, stud_array.size()):
+	#	students_pos.append(stud_array[i].get_pos())
+	
+	#print("students_pos[0] ", students_pos[0])
+	for s in students:
+		stud_array.append(s)
+	
+	
+	
 	set_process(true)
 	
 
-	
-	
 func _process(delta):
-	var student_pos = get_node("student").get_pos()
+	
+	# trying to make a loop of all students in a group and added them to array
+
+	
+		
+	#var student_pos = get_node("student").get_pos()
+	var students_pos = []
+	for i in range(0, stud_array.size()):
+		students_pos.append(stud_array[i].get_pos())
+	#var student_pos = stud_array[0].get_pos()
+	#var student_pos2 = stud_array[1].get_pos()
+		# stud_array.size() = 2
+
+	
+	#print("student pos ", student_pos)
+	print("array of students pos", students_pos)
+	#print("second of array ", student_pos2)
+	
+	
+
+
+	
 	var teacher_pos = get_node("teacher").get_pos()
 	var teacher_rect = Rect2(teacher_pos - teacher_size*0.5, teacher_size)
 	
-	student_pos += direction * student_speed * delta
+	students_pos[0] += direction * student_speed * delta
 
 		# FOR TEST ----
 
 	#--------------
 	
 	# Pause, change direction and increase speed when come to teacher and check student's answer
-	if ((teacher_rect.has_point(student_pos))):
+	if ((teacher_rect.has_point(students_pos[0]))):
 		dialog.show()
 
-		student_pos = get_node("student").get_pos()
-		if (b_right.is_pressed() or b_wrong.is_pressed()):
+		students_pos[0] = get_node("student").get_pos()
+		if (b_correct.is_pressed() or b_wrong.is_pressed()):
 			direction.x = -direction.x
 			student_speed *= 1.1
+			
+			
+	
+		
+		
 		
 	
-	get_node("student").set_pos(student_pos)
+	get_node("student").set_pos(students_pos[0])
+	
 	
 
 
